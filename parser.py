2e1028c608058a20210855e69ebf4f00b13ad592
Arun G <mdrk07@gmail.com>
2014-06-16 10:06:35 -0700

    Initial commit

---
+++ parser.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+"""
+E-mail Parser
+@author Arun George, 2014
+Python
+"""
+
+# import regex lib
+import re
+
+def main():
+    """Parses headers from an email file using regular expressions."""
+
+    # set input file
+    file_input  = "C:\\Users\\George\\Desktop\\python - parser\\email2.txt"
+    global email_file
+
+    # try to open the file
+    try:
+        email_file  = open(file_input,"r")
+    except:
+        print("File can't be opened to read! - ", file_input)
+        exit()
+
+    print("-" * 50) # line dash
+
+    # read sequence of lines and print output
+    for line in email_file:
+        line = line.rstrip()
+
+        if re.search('^From:', line) or \
+            re.search('^To:',line) or \
+            re.search('^Subject:',line) or \
+            re.search('^Date:',line)  or \
+            re.search('^Content-Type:',line):
+
+            print(line) # display the final output
+
+
+    email_file.close()
+
+main()
